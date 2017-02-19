56d7feb25242d7efcae81e897b44735e710ab593
Leevi Lehtonen <leevi.k.lehtonen@helsinki.fi>
2017-02-20 00:11:38 +0200

    Html modeleja luotu

--- src/main/java/tikape/foorumi/Main.java
+++ src/main/java/tikape/foorumi/Main.java
@@ -2,6 +2,7 @@ package tikape.foorumi;
 
 import java.util.HashMap;
 import spark.ModelAndView;
+import spark.Spark;
 import static spark.Spark.*;
 import spark.template.thymeleaf.ThymeleafTemplateEngine;
 import tikape.foorumi.database.Database;
@@ -9,14 +10,18 @@ import tikape.foorumi.database.Database;
 public class Main {
 
     public static void main(String[] args) throws Exception {
+        Spark.staticFileLocation("/public");
         Database database = new Database("jdbc:sqlite:foorumi.db");
         database.init();
 
         get("/", (req, res) -> {
             HashMap map = new HashMap<>();
-            map.put("viesti", "tervehdys");
-
             return new ModelAndView(map, "index");
+        }, new ThymeleafTemplateEngine());
+        
+        get("/alue", (req, res) -> {
+            HashMap map = new HashMap<>();
+            return new ModelAndView(map, "alue");
         }, new ThymeleafTemplateEngine());
     }
 }
